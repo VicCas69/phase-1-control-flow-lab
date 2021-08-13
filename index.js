e199183903031a3ce4f37791bf9b6baeb3c73d40
victor castro <victor_castro_01@hotmail.com>
2021-08-12 20:57:24 -0400

    commit tree 1f8d0f6bdef3bf88ba52e3dd43b03fbd2246a11a

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceTravel){
+  let result
+
+  if (distanceTravel <= 400){
+   result = "This one is on me!" ;// rite your code here!
+}
+else if (distanceTravel > 2000 && distanceTravel < 2501){
+  result = "I will gladly take your thirty bucks.";
+}
+else if (distanceTravel >= 2501){
+  result = "No can do.";
+}
+  
+  return result;
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){ 
+  let location;
+  city === "NYC" ? location = "Ok, sounds good." : location = "No go."; 
+  return location;
+//if (city === "NYC"){ location = "Ok, sounds good."}
+//else{ location = "No go.";}
+//return location;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let money;
+  switch(tip){
+    case 'generous' : 
+      money = 'Thank you so much.';
+      break;
+    case 'not as generous' : 
+      money = 'Thank you.'; 
+      break;
+    default :
+      money = 'Bye.';
+      break;
+  }
+  return money;
 }
